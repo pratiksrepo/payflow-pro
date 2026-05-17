d12623ca70a28158a9c1f673a70a16f4f1e7706b
pratikshintre <pratikshintre2002@gmail.com>
2026-05-17 20:24:31 +0530

    Day 2 backend microservices setup

---
+++ backend/services/PayFlow.AuthService/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PayFlow.AuthService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet("health")]
+    public IActionResult Health()
+    {
+        return Ok("Auth Service Running");
+    }
+}
---
+++ backend/shared/PayFlow.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PayFlow.Shared;
+
+public class Class1
+{
+
+}
