aec3a37511504c3b082db5df05a7efeedb5c9704
pratikshintre <pratikshintre2002@gmail.com>
2026-07-29 22:50:12 +0530

    Fix API base URL

--- frontend/payflow-ui/src/services/apiClient.ts
+++ frontend/payflow-ui/src/services/apiClient.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://localhost:7056/api",
-    headers: {
-        "Content-Type": "application/json"
-    }
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 export default apiClient;
